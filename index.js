05d3640cb76f0b2e1df2cb1a69730b92be0108d7
naraekim <narae900@naver.com>
2022-02-08 11:36:59 +0900

    any,unknown

--- index.js
+++ index.js
@@ -17,3 +17,39 @@ var User = /** @class */ (function () {
     }
     return User;
 }());
+// Union Type
+// 타입2개 이상 합친 새로운 타입 만들기
+var 회원 = 1234;
+회원 = 'kim';
+var 회원들 = [1, '2', 3];
+var 오브젝트 = { a: '123' };
+// any타입 
+// 타입실드 해제문법
+// 일반 JS변수로 만들고 싶을 때
+// 타입 관련 버그가 나도 잡아주지 않음
+var 이름2;
+이름2 = 123;
+이름2 = [];
+// unknown 타입 
+// any보다 안전
+// unknown은 number타입이 아님
+var 이름3;
+// 간단한 수학연산도 타입을 맞춰야 함
+var 나이2;
+// string타입 +1 (허용)
+// number타입 +1 (허용)
+// string | number +1 (안됨)
+// 나이2 + 1; 안됨 
+// 변수 타입 지정해보기 1
+var user = 'kim';
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+// 타입 지정해보기  답 2
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
--- index.ts
+++ index.ts
@@ -36,4 +36,59 @@ class User {
 }
 
 
+// Union Type
+// 타입2개 이상 합친 새로운 타입 만들기
+let 회원 :number | string = 1234;
+회원 = 'kim';
 
+let 회원들 :(number | string)[] = [1, '2', 3];
+let 오브젝트 :{ a : string | number } = { a : '123'}
+
+
+// any타입 
+// 타입실드 해제문법
+// 일반 JS변수로 만들고 싶을 때
+// 타입 관련 버그가 나도 잡아주지 않음
+let 이름2 :any;
+이름2 = 123;
+이름2 = [];
+
+
+// unknown 타입 
+// any보다 안전
+// unknown은 number타입이 아님
+let 이름3 :unknown;
+
+// 간단한 수학연산도 타입을 맞춰야 함
+
+let 나이2 :string | number;
+
+// string타입 +1 (허용)
+// number타입 +1 (허용)
+// string | number +1 (안됨)
+
+// 나이2 + 1; 안됨 
+
+
+
+// 변수 타입 지정해보기 1
+let user :string = 'kim';
+let age :number | undefined  = undefined;
+let married :boolean = false; 
+let 철수 :(string|number|boolean )[] = [user, age, married];
+
+
+// 타입 지정해보기  답 2
+let 학교 : {
+    score : (number | boolean)[],
+    teacher : string,
+    friend : string | string[]
+}
+
+= {
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
